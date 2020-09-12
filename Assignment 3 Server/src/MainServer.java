4814039f55334a4149b9fe13b9fbe1c8a4c7c40e
pradyumnap <pradyumnaraje.patil@gmail.com>
2020-09-12 22:48:36 +0530

    MultiThreading

--- Assignment 3 Server/src/MainServer.java
+++ Assignment 3 Server/src/MainServer.java
@@ -1,9 +1,15 @@
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.net.*;
-import java.util.*;
-import java.util.concurrent.Executor;
+import java.util.Hashtable;
+import java.util.Properties;
+import java.util.Vector;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+
 
 
 
@@ -30,7 +36,7 @@ class OrdersHistory implements Serializable {
 }
 
 
-public class MainServer implements Runnable {
+public class MainServer {
     public static ServerSocket serverSocket;
     public static Socket socket;
     public static ObjectInputStream userObjectInputStream;
@@ -39,6 +45,8 @@ public class MainServer implements Runnable {
     public static Boolean isLogged;
     public static Code code = new Code();
 
+
+    ///
     public static ObjectOutputStream objectOutputStream;
     public static DataInputStream dataInputStream;
 
@@ -47,46 +55,46 @@ public class MainServer implements Runnable {
     public static RestraurentMenu restraurentMenu = new RestraurentMenu();
     public static String[] dishItems = {"SIZZLER", "SOUP","ROTI","MAINCOURSE","RICE","FRUITSALAD", "DESSERT","BEVERAGE"};
 
+
+
     public static ExecutorService executorService;
 
-
-    public static void main(String[] args){
+    public static void main(final String[] args){
         try {
             isLogged = false;
             registeredUsers.users = new Properties();
 
             ordersHistory = new OrdersHistory();
 
-            
-
             addStaticUser();
             addMenuItems();
-
-            // MULTI THREADING IN JAVE
-            executorService = Executors.newFixedThreadPool(5);
+            
 
             // ESTABLISH A SERVER SOCKET PROGRAM
             serverSocket = new ServerSocket(8080);
+    
+            executorService = Executors.newFixedThreadPool(5);
+                
             while(true){
                 getActiviyToPerform();
             }
-        } catch(Exception e){
-                e.printStackTrace();
+            
+            } catch(final Exception e){
+            e.printStackTrace();
         }
+
     }
 
-    public void run(){
-        System.out.println("Thread running is : " + Thread.currentThread().getName());
-    }
-
-    public static void loginFunc(){
+    public static void loginFunc(final ObjectOutputStream oos){
         try{
+            System.out.println("Thread : " + Thread.currentThread().getName());
+
             // ACCEPT ANY REQUEST THAT COMES
             userObjectInputStream = new ObjectInputStream(socket.getInputStream());
 
-            LoginUser user = (LoginUser) userObjectInputStream.readObject();
+            final LoginUser user = (LoginUser) userObjectInputStream.readObject();
 
-            ServerResponse serverResponse = new ServerResponse();
+            final ServerResponse serverResponse = new ServerResponse();
 
             // WHEN THE USERNAME AND THE PASSWORD EXISTS
             if(registeredUsers.users.getProperty(user.USERNAME) != null && registeredUsers.users.getProperty(user.USERNAME).equals(user.PASSWORD)){
@@ -104,12 +112,12 @@ public class MainServer implements Runnable {
                 serverResponse.success = false;
                 System.out.println("Login Failed\n");
             }
-            objectOutputStream.writeObject(serverResponse);
+            oos.writeObject(serverResponse);
 
 
 
             socket.close();
-        } catch(Exception e){
+        } catch(final Exception e){
             e.printStackTrace();
         }  
     }
@@ -117,32 +125,32 @@ public class MainServer implements Runnable {
 
     public static void getActiviyToPerform(){
         try {
-            executorService.execute(new MainServer());
-
             System.out.println("Waiting For Client connection for Menu ...\n");
             socket = serverSocket.accept();
             System.out.println("Connection between Client And Server established for Menu...\n");
 
-            dataInputStream = new DataInputStream(socket.getInputStream());
+            final Tasks tasks = new Tasks(socket);
             objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
 
-
-            Integer inputCode = dataInputStream.readInt();
-            handleTheUserChoice(inputCode);
+            executorService.execute(tasks);
 
 
-        } catch(Exception e){
+
+            //socket.close();
+        } catch(final Exception e){
             e.printStackTrace();
         }
 
     }
 
-    public static void handleTheUserChoice(Integer choice){
-        try {
 
+    
+    public static void handleTheUserChoice(final Integer choice){
+        try {
+            System.out.println("Thread : " + Thread.currentThread().getName());
             if(choice.equals(code.login)){
                 System.out.println("Login Attempted from client\n");
-                loginFunc();
+                loginFunc(objectOutputStream);
             } else if(choice.equals(code.getRegisteredUsers)){
                 System.out.println("Request for all users from client\n");
 
@@ -156,12 +164,13 @@ public class MainServer implements Runnable {
                 objectOutputStream.writeObject(restraurentMenu);
 
                 System.out.println("Food Menu request completed\n");
+                socket.close();
 
             } else if(choice.equals(code.addSizzler)) {
                 System.out.println("Request Add Sizzler from client\n");
 
-                ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
-                Dishes dish = (Dishes) dishFromClient.readObject();
+                final ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
+                final Dishes dish = (Dishes) dishFromClient.readObject();
                 
                 restraurentMenu.Dishes.get(dishItems[0]).add(dish);
 
@@ -170,8 +179,8 @@ public class MainServer implements Runnable {
             }  else if(choice.equals(code.addSoup)) {
                 System.out.println("Request Add Soup from client\n");
 
-                ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
-                Dishes dish = (Dishes) dishFromClient.readObject();
+                final ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
+                final Dishes dish = (Dishes) dishFromClient.readObject();
                 
                 restraurentMenu.Dishes.get(dishItems[1]).add(dish);
                 System.out.println("Add Soup request completed\n");
@@ -179,8 +188,8 @@ public class MainServer implements Runnable {
             } else if(choice.equals(code.addRoti)) {
                 System.out.println("Request Add Roti from client\n");
 
-                ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
-                Dishes dish = (Dishes) dishFromClient.readObject();
+                final ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
+                final Dishes dish = (Dishes) dishFromClient.readObject();
                 
                 restraurentMenu.Dishes.get(dishItems[2]).add(dish);
                 System.out.println("Add Roti request completed\n");
@@ -188,8 +197,8 @@ public class MainServer implements Runnable {
             } else if(choice.equals(code.addMainCourse)) {
                 System.out.println("Request Add Main Course from client\n");
 
-                ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
-                Dishes dish = (Dishes) dishFromClient.readObject();
+                final ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
+                final Dishes dish = (Dishes) dishFromClient.readObject();
                 
                 restraurentMenu.Dishes.get(dishItems[3]).add(dish);
                 System.out.println("Add Main Course request completed\n");
@@ -197,8 +206,8 @@ public class MainServer implements Runnable {
             } else if(choice.equals(code.addRice)){
                 System.out.println("Request Add Rice from client\n");
 
-                ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
-                Dishes dish = (Dishes) dishFromClient.readObject();
+                final ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
+                final Dishes dish = (Dishes) dishFromClient.readObject();
                 
                 restraurentMenu.Dishes.get(dishItems[4]).add(dish);
                 System.out.println("Add Rice request completed\n");
@@ -206,8 +215,8 @@ public class MainServer implements Runnable {
             } else if(choice.equals(code.addFruitSalad)){
                 System.out.println("Request Add Fruit Salad from client\n");
 
-                ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
-                Dishes dish = (Dishes) dishFromClient.readObject();
+                final ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
+                final Dishes dish = (Dishes) dishFromClient.readObject();
                 
                 restraurentMenu.Dishes.get(dishItems[5]).add(dish);
                 System.out.println("Add Fruit Salad request completed\n");
@@ -215,8 +224,8 @@ public class MainServer implements Runnable {
             } else if(choice.equals(code.addDessert)){
                 System.out.println("Request Add Dessert from client\n");
 
-                ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
-                Dishes dish = (Dishes) dishFromClient.readObject();
+                final ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
+                final Dishes dish = (Dishes) dishFromClient.readObject();
                 System.out.println(dish.nameOfItem);
                 
                 restraurentMenu.Dishes.get(dishItems[6]).add(dish);
@@ -225,8 +234,8 @@ public class MainServer implements Runnable {
             } else if(choice.equals(code.addBeverage)){
                 System.out.println("Request Add Beverage from client\n");
 
-                ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
-                Dishes dish = (Dishes) dishFromClient.readObject();
+                final ObjectInputStream dishFromClient = new ObjectInputStream(socket.getInputStream());
+                final Dishes dish = (Dishes) dishFromClient.readObject();
                 System.out.println(dish.nameOfItem);
                 
                 restraurentMenu.Dishes.get(dishItems[7]).add(dish);
@@ -235,11 +244,12 @@ public class MainServer implements Runnable {
             } else if(choice.equals(code.placeOrder)) {
                 System.out.println("Request Place Order from client\n");
 
-                ObjectInputStream orderFromClient = new ObjectInputStream(socket.getInputStream());
-                Order currentOrder = (Order) orderFromClient.readObject();
+                final ObjectInputStream orderFromClient = new ObjectInputStream(socket.getInputStream());
+                final Order currentOrder = (Order) orderFromClient.readObject();
 
                 ordersHistory.orders.add(currentOrder);
                 System.out.println("Place Order request completed\n");
+                socket.close();
 
             } else if(choice.equals(code.orderHistory)){
                 System.out.println("Request Order History from client\n");
@@ -248,13 +258,12 @@ public class MainServer implements Runnable {
                 objectOutputStream.writeObject(ordersHistory);
                 System.out.println("Order History request completed\n");
 
-
             } else if(choice.equals(code.editOrder)) {
                 System.out.println("Request Edit Previous Order from client\n");
 
 
-                ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-                Order editedOrder = (Order) ois.readObject();
+                final ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                final Order editedOrder = (Order) ois.readObject();
 
                 for(int i=0;i<ordersHistory.orders.size();i++){
                     if(ordersHistory.orders.get(i).orderID.equals(editedOrder.orderID)){
@@ -267,41 +276,38 @@ public class MainServer implements Runnable {
 
             } else if(choice.equals(code.chat)){
                 System.out.println("Chat Box Request received\n");
-                ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+                final ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
 
                 Chat.chatWithClient(objectOutputStream,objectInputStream);
-            }
-
-
-
-            socket.close();
-
-
-        } catch(Exception e){
+            }           
+            
+            System.out.println("Reaches");
+            //socket.close();
+        } catch(final Exception e){
             e.printStackTrace();
         }
     }
 
-    
+   
 
     public static void addMenuItems(){
         // SIZZLERS
-        String[] SIZZLERS = {"Veg Tawa Sizzler","Veg Ala Kiev Sizzler","Soya Chaap Sizzler","Cheese Cutlet Sizzler"};
-        Float[] SIZZLERPRICE = {600f,600f,650f,600f};
+        final String[] SIZZLERS = {"Veg Tawa Sizzler","Veg Ala Kiev Sizzler","Soya Chaap Sizzler","Cheese Cutlet Sizzler"};
+        final Float[] SIZZLERPRICE = {600f,600f,650f,600f};
         restraurentMenu.Dishes.put(dishItems[0], new Vector<Dishes>());
         for(int i=0;i<SIZZLERS.length;i++){
-            Dishes dish = new Dishes();
+            final Dishes dish = new Dishes();
             dish.nameOfItem=SIZZLERS[i];
             dish.price=SIZZLERPRICE[i];
             restraurentMenu.Dishes.get(dishItems[0]).add(dish);
         }
 
         // SOUP
-        String[] SOUP = {"Tomato Soup","Palak Soup","Manchaw Soup","Sweet Corn Soup"};
-        Float[] SOUPPRICE = {80f,80f,95f,95f};
+        final String[] SOUP = {"Tomato Soup","Palak Soup","Manchaw Soup","Sweet Corn Soup"};
+        final Float[] SOUPPRICE = {80f,80f,95f,95f};
         restraurentMenu.Dishes.put(dishItems[1], new Vector<Dishes>());
         for(int i=0;i<SOUP.length;i++){
-            Dishes dish = new Dishes();
+            final Dishes dish = new Dishes();
             dish.nameOfItem=SOUP[i];
             dish.price=SOUPPRICE[i];
             restraurentMenu.Dishes.get(dishItems[1]).add(dish);
@@ -309,22 +315,22 @@ public class MainServer implements Runnable {
         
 
         // ROTI
-        String[] ROTI = {"Roti","ButterRoti","Naan","Butter Naan","Paratha","Butter Paratha","Chapati","Butter Chapati"};
-        Float[] ROTIPRICE = {15f,20f,25f,30f,25f,30f,10f,15f};
+        final String[] ROTI = {"Roti","ButterRoti","Naan","Butter Naan","Paratha","Butter Paratha","Chapati","Butter Chapati"};
+        final Float[] ROTIPRICE = {15f,20f,25f,30f,25f,30f,10f,15f};
         restraurentMenu.Dishes.put(dishItems[2], new Vector<Dishes>());
         for(int i=0;i<ROTI.length;i++){
-            Dishes dish = new Dishes();
+            final Dishes dish = new Dishes();
             dish.nameOfItem=ROTI[i];
             dish.price=ROTIPRICE[i];
             restraurentMenu.Dishes.get(dishItems[2]).add(dish);
         }
         
         // MAIN COURSE
-        String[] MAINCOURSE = {"Veg Machurian","Paneer Machurian","Veg Schezwan","Paneer Chilly","Veg Crispy"};
-        Float[] MAINCOURSEPRICE = {150f,170f,170f,170f,160f};
+        final String[] MAINCOURSE = {"Veg Machurian","Paneer Machurian","Veg Schezwan","Paneer Chilly","Veg Crispy"};
+        final Float[] MAINCOURSEPRICE = {150f,170f,170f,170f,160f};
         restraurentMenu.Dishes.put(dishItems[3], new Vector<Dishes>());
         for(int i=0;i<MAINCOURSE.length;i++){
-            Dishes dish = new Dishes();
+            final Dishes dish = new Dishes();
             dish.nameOfItem=MAINCOURSE[i];
             dish.price=MAINCOURSEPRICE[i];
             restraurentMenu.Dishes.get(dishItems[3]).add(dish);
@@ -332,33 +338,33 @@ public class MainServer implements Runnable {
         
 
         // RICE
-        String[] RICE = {"Veg Biryani","Paneer Biryani","Hyderabadi Biryani","Veg Pulav"};
-        Float[] RICEPRICE = {150f,170f,170f,150f};
+        final String[] RICE = {"Veg Biryani","Paneer Biryani","Hyderabadi Biryani","Veg Pulav"};
+        final Float[] RICEPRICE = {150f,170f,170f,150f};
         restraurentMenu.Dishes.put(dishItems[4], new Vector<Dishes>());
         for(int i=0;i<RICE.length;i++){
-            Dishes dish = new Dishes();
+            final Dishes dish = new Dishes();
             dish.nameOfItem=RICE[i];
             dish.price=RICEPRICE[i];
             restraurentMenu.Dishes.get(dishItems[4]).add(dish);
         }
 
         // FRUIT SALAD
-        String[] FRUITSALAD = {"Green Salad","Cheese Cherry Pineapple"};
-        Float[] FRUITSALADPRICE = {90f,135f};
+        final String[] FRUITSALAD = {"Green Salad","Cheese Cherry Pineapple"};
+        final Float[] FRUITSALADPRICE = {90f,135f};
         restraurentMenu.Dishes.put(dishItems[5], new Vector<Dishes>());
         for(int i=0;i<FRUITSALAD.length;i++){
-            Dishes dish = new Dishes();
+            final Dishes dish = new Dishes();
             dish.nameOfItem=FRUITSALAD[i];
             dish.price=FRUITSALADPRICE[i];
             restraurentMenu.Dishes.get(dishItems[5]).add(dish);
         }
         
         // DESSERT
-        String[] DESSERT = {"Vanilla Ice Cream","Mango Ice Cream","Strawberry Ice Cream","Chocolate Ice Cream"};
-        Float[] DESSERTPRICE = {30f,35f,30f,45f};
+        final String[] DESSERT = {"Vanilla Ice Cream","Mango Ice Cream","Strawberry Ice Cream","Chocolate Ice Cream"};
+        final Float[] DESSERTPRICE = {30f,35f,30f,45f};
         restraurentMenu.Dishes.put(dishItems[6], new Vector<Dishes>());
         for(int i=0;i<DESSERT.length;i++){
-            Dishes dish = new Dishes();
+            final Dishes dish = new Dishes();
             dish.nameOfItem=DESSERT[i];
             dish.price=DESSERTPRICE[i];
             restraurentMenu.Dishes.get(dishItems[6]).add(dish);
@@ -366,11 +372,11 @@ public class MainServer implements Runnable {
 
 
         // BEVERAGE
-        String[] BEVERAGE = {"Fresh Lime Juice","Fresh Lime Soda","Soft Drinks","Mango Lassi"};
-        Float[] BEVERAGEPRICE = {50f,60f,30f,20f};
+        final String[] BEVERAGE = {"Fresh Lime Juice","Fresh Lime Soda","Soft Drinks","Mango Lassi"};
+        final Float[] BEVERAGEPRICE = {50f,60f,30f,20f};
         restraurentMenu.Dishes.put(dishItems[7], new Vector<Dishes>());
         for(int i=0;i<BEVERAGE.length;i++){
-            Dishes dish = new Dishes();
+            final Dishes dish = new Dishes();
             dish.nameOfItem=BEVERAGE[i];
             dish.price=BEVERAGEPRICE[i];
             restraurentMenu.Dishes.get(dishItems[7]).add(dish);
